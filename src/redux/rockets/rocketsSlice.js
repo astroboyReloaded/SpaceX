061f29fea03588ff2388960709dd21af02b4d792
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-06-20 16:45:17 -0600

    Create rocketSlice shell

---
+++ src/redux/rockets/rocketsSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  rockets: [],
+};
+
+const rocketsSlice = createSlice({
+  name: 'rockets',
+  initialState,
+  extraReducers: {},
+});
+
+export default rocketsSlice.reducer;
