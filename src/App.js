bb80703ce0b4cb4b4e24c24d8d65955e78a9654f
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-06-20 16:26:53 -0600

    MyProfile setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
+import MyProfile from './components/myProfile/MyProfile';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Nav />}>
       <Route index element={<h1>Rockets</h1>} />
       <Route path="/missions" element={<h1>Missions</h1>} />
-      <Route path="/profile" element={<h1>My Profile</h1>} />
+      <Route path="/profile" element={<MyProfile />} />
       <Route path="*" element={<h1>Page Not Found</h1>} />
     </Route>
   </Routes>
