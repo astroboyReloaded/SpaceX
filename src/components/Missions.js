30c8fdaf735dc63e64e551fb398e5fc50cc35614
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-06-21 16:12:03 -0600

    handleMission

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchMissions } from '../redux/missions/missionsSlice';
+import { fetchMissions, handleMission } from '../redux/missions/missionsSlice';
 // import Badge from './Badge';
 // import ReserveBtn from './ReserveBtn';
 
 const Missions = () => {
   const dispatch = useDispatch();
   const { missions } = useSelector((state) => state.missions);
+  console.log(missions);
 
   useEffect(() => {
     if (missions.length) return;
@@ -25,6 +26,7 @@ const Missions = () => {
           <tr key={mission.id}>
             <td>{mission.name}</td>
             <td>{mission.description}</td>
+            <button type="button" onClick={() => dispatch(handleMission(mission.id))}>{mission.reserved ? 'Leave Mission' : 'Join Mission'}</button>
             {/* <td><Badge /></td>
             <td><ReserveBtn /></td> */}
           </tr>
