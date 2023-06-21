9a9ca259c6762f6c539a47d3ca253df6d87f8de7
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-06-20 18:08:12 -0600

    Create Rockets compnent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Nav from './components/Nav';
+import Rockets from './components/Rockets';
 import MyProfile from './components/myProfile/MyProfile';
 
 const App = () => (
   <Routes>
     <Route path="/" element={<Nav />}>
       <Route index element={<h1>Rockets</h1>} />
-      <Route path="/missions" element={<h1>Missions</h1>} />
+      <Route path="/missions" element={<Rockets />} />
       <Route path="/profile" element={<MyProfile />} />
       <Route path="*" element={<h1>Page Not Found</h1>} />
     </Route>
