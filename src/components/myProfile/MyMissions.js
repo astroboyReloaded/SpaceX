26312e6d03ba583c611360c11c8f67c9cdc1745d
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-06-22 11:25:56 -0600

    Use filter to display reserved/joined

--- src/components/myProfile/MyMissions.js
+++ src/components/myProfile/MyMissions.js
@@ -3,11 +3,12 @@ import style from '../../styles/myProfile.module.css';
 
 const MyMissions = () => {
   const { missions } = useSelector((state) => state.missions);
+  const joinedMissions = missions.filter((mission) => mission.reserved);
 
   return (
     <section className={style.sectionContainer}>
       <h1>MyItems</h1>
-      {missions.map((mission) => mission.reserved && (
+      {joinedMissions.map((mission) => (
         <article className={style.article} key={mission.id}>
           <h1>{mission.name}</h1>
         </article>
--- src/components/myProfile/MyRockets.js
+++ src/components/myProfile/MyRockets.js
@@ -3,11 +3,12 @@ import style from '../../styles/myProfile.module.css';
 
 const MyRockets = () => {
   const { rockets } = useSelector((state) => state.rockets);
+  const reservedRockets = rockets.filter((rocket) => rocket.reserved);
 
   return (
     <section className={style.sectionContainer}>
       <h1>MyItems</h1>
-      {rockets.map((rocket) => rocket.reserved && (
+      {reservedRockets.map((rocket) => (
         <article className={style.article} key={rocket.id}>
           <h1>{rocket.name}</h1>
         </article>
