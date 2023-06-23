4f824a6a625978dbb41873fdf013fb3dfc7ac396
AstroboyReloaded <astroboy.reloaded@gmail.com>
2023-06-23 13:12:13 -0600

    eslint

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import renderer from 'react-test-renderer';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import Store from '../redux/Store';
 import App from '../App';
